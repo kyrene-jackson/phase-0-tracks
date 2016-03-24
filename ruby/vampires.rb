9ce86035cc6832eb922e0d51c3695691193a024e
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-03-24 09:31:39 -0500

    Complete 4.4- Release 1

---
+++ ruby/vampires.rb
@@ -0,0 +1,7 @@
+puts "What is your name?"
+name = gets.chomp
+puts "How old are you? What year were you born?"
+age_birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
+garlic_bread_pref = gets.chomp
+puts "Would you like to enroll in the company’s health insurance? (y/n)"
