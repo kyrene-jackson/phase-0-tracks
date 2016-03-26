f93ca61e757cfefa1d78e441bfc750c5c29935b0
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-03-25 22:59:42 -0500

    Create health insurance pref conditional statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,10 +5,10 @@ puts "How old are you?"
 age = gets.chomp.to_i
 puts "What year were you born?"
 year_born = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you? (Yes or No)"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes or no)"
 garlic_bread_pref = gets.chomp.downcase
-puts "Would you like to enroll in the company's health insurance?"
-health_ins = gets.chomp
+puts "Would you like to enroll in the company's health insurance? (yes or no)"
+health_ins = gets.chomp.downcase
 # Release 2 detection logic
 #if the employee got their age right AND is willing to eat garlic bread OR sign up for insurance => "Probably not a vampire"
 
@@ -21,5 +21,21 @@ else
   age_verify = false
 end
 
+#check if employee is willing to eat garlic bread
+will_eat_garlic_bread = false
+if garlic_bread_pref == "yes"
+  will_eat_garlic_bread = true
+else
+  will_eat_garlic_bread = false
+end
+
+#check if employee wants to sign up for health insurance
+wants_health_insurance = false
+if health_ins == "yes"
+  wants_health_insurance = true
+else
+  wants_health_insurance = false
+end
+
 #if the employee got their age right AND is willing to eat garlic bread OR sign up for insurance
-if age_verify &&
+if age_verify && will_eat_garlic_bread
