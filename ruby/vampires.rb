d16189bcd22d8106782e0311e3160691b94abd7d
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-03-25 22:56:09 -0500

    Create garlic bread pref conditional statements

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,8 +5,8 @@ puts "How old are you?"
 age = gets.chomp.to_i
 puts "What year were you born?"
 year_born = gets.chomp.to_i
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-garlic_bread_pref = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (Yes or No)"
+garlic_bread_pref = gets.chomp.downcase
 puts "Would you like to enroll in the company's health insurance?"
 health_ins = gets.chomp
 # Release 2 detection logic
@@ -18,5 +18,8 @@ calculated_year_born = (2016 - age)
 if calculated_year_born == year_born
   age_verify = true
 else
-  age_verify = false  
+  age_verify = false
 end
+
+#if the employee got their age right AND is willing to eat garlic bread OR sign up for insurance
+if age_verify &&
