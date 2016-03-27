ec477f398a23f62bb7051e1360e24b5a115e8705
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-03-27 03:31:14 -0500

    Complete scenario conditionals, create case statement for survey result

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,7 +2,7 @@
 # Gather basic data
 #---------------------------
 puts "What is your name?"
-employee_name = gets.chomp
+employee_name = gets.chomp.downcase
 puts "How old are you?"
 employee_age = gets.chomp.to_i
 puts "What year were you born?"
@@ -23,12 +23,46 @@ wants_health_ins = gets.chomp.downcase
 calculated_age = 2016 - employee_birth_year
 # Set conditional statement
 if calculated_age == employee_age && (wants_garlic_bread == "yes" || wants_health_ins == "yes")
-  puts "Probably not a vampire."
+  #puts "Probably not a vampire."
+  survey_result = "one"
 end
 
 #---------------------------
 # Scenario 2
 # If the employee got their age wrong, and hates garlic bread or waives insurance, the result is “Probably a vampire.”
 if calculated_age != employee_age && (wants_garlic_bread == "no" || wants_health_ins == "no")
-  puts "Probably not a vampire"
+  #puts "Probably a vampire"
+  survey_result = "two"
+end
+
+#---------------------------
+# Scenario 3
+# If the employee got their age wrong, hates garlic bread, and doesn’t want insurance, the result is “Almost certainly a vampire.”
+
+if calculated_age != employee_age && (wants_garlic_bread == "no" && wants_health_ins == "no")
+  #puts "Almost certainly a vampire"
+  survey_result = "three"
+end
+
+#---------------------------
+# Scenario 4
+# Even if the employee is an amazing liar otherwise, anyone going by the name of “Drake Cula” or “Tu Fang” is clearly a vampire, because come on. In that case, you should print “Definitely a vampire.”
+
+if employee_name == "drake cula" || employee_name == "tu fang"
+  # puts "Definitely a vampire"
+  survey_result = "four"
+end
+#---------------------------
+
+case survey_result
+when "one"
+  puts "Probably not a vampire."
+when "two"
+  puts "Probably a vampire."
+when "three"
+  puts "Almost certainly a vampire."
+when "four"
+  puts "Definitely a vampire."
+else
+  puts "Results inconclusive."
 end
