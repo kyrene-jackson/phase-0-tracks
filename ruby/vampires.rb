e1d6f9aa4e8d6321d16b33ee72c0d9597c041d3f
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-03-27 00:14:40 -0500

    Create method for age verification

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -4,9 +4,11 @@
 puts "What is your name?"
 employee_name = gets.chomp
 puts "How old are you?"
-employee_age = gets.chomp
+$employee_age = gets.chomp
+# Employee age set as global variable for verify method
 puts "What year were you born?"
-employee_birth_year = gets.chomp
+$employee_birth_year = gets.chomp
+# Employee birth year set as global variable for verify method
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 wants_garlic_bread = gets.chomp
 puts "Would you like to enroll in the company’s health insurance?"
@@ -15,3 +17,13 @@ wants_health_ins = gets.chomp
 # Release 2
 # Detection logic
 #---------------------------
+# Scenario 1
+#If the employee got their age right, and is willing to eat garlic bread or sign up for insurance, the result is “Probably not a vampire.”
+
+# Determine if employee's age is mathematically correct
+# Create method to perform calculation
+def age_verify(age, birthyear)
+  birthyear.to_i
+  age.to_i
+  2016 - birthyear == age
+end
