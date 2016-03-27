83a358aa1190cbec206e4eaffb2ab9cd878d8f62
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-03-26 23:41:51 -0500

    Complete basic data input

---
+++ ruby/vampires.rb
@@ -0,0 +1,17 @@
+# Release 1
+# Gather basic data
+#---------------------------
+puts "What is your name?"
+employee_name = gets.chomp
+puts "How old are you?"
+employee_age = gets.chomp
+puts "What year were you born?"
+employee_birth_year = gets.chomp
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+wants_garlic_bread = gets.chomp
+puts "Would you like to enroll in the company’s health insurance?"
+wants_health_ins = gets.chomp
+#---------------------------
+# Release 2
+# Detection logic
+#---------------------------
