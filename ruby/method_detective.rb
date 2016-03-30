eddf456a1175f9de6347875a016f7dcbcff6626c
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-03-29 22:47:32 -0500

    Complete 5.1

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,46 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
+“iNvEsTiGaTiOn”.swapcase
 # => “InVeStIgAtIoN”
 
-# “zom”.<???>
+ “zom”.replace "zoom"
 # => “zoom”
 
-# “enhance”.<???>
+ “enhance”.center(20)
 #=> “    enhance    ”
 
-# “Stop! You’re under arrest!”.<???>
+ “Stop! You’re under arrest!”.upcase
 # => “STOP! YOU’RE UNDER ARREST!”
 
-# “the usual”.<???>
+ my_string = "the usual "
+ my_string.concat("suspects")
+ # or
+ my_string_two = "the usual "
+ my_string_two << "suspects"
 #=> “the usual suspects”
 
-#“ suspects”.<???>
+“ suspects”.insert(0, "the usual")
 #=> “the usual suspects”
 
-# “The case of the disappearing last letter”.<???>
+ “The case of the disappearing last letter”.chop
 # => “The case of the disappearing last lette”
 
-# “The mystery of the missing first letter”.<???>
+ example = “The mystery of the missing first letter”
+ example.slice!(0)
+ p example
+ #or
+ example_2 = "“The mystery of the missing first letter”"
+ example_2[1..-1]
 # => “he mystery of the missing first letter”
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
+“Elementary  ,    my   dear      Watson!”.squeeze(" ")""
 # => “Elementary, my dear Watson!”
 
-#“z”.<???>
-# => 122 
+“z”.ord
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
+# Answer: 122 is the ASCII representation of the lowercase z.i
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
+“How many times does the letter ‘a’ appear in this string?”.count "a"
 #=> 4
