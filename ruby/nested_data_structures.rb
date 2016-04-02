9cc82f11b121bf03ca0f70ef3072b08a2b0aba5d
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-02 14:02:46 -0500

    Finish first level of nested data strcture

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,56 @@
+# 5.5- Release 2
+# Design and build a nested data structure to represent a real-world construct.
+# Once you've built it, print a few individual pieces of deeply nested data from the structure, showing that you know how to use multiple indexes or hash keys (or both) to access nested items.
+#-----------------------------------------
+# Construct: Electronics Store!
+
+electronics_store = {
+  aisle_one: {
+    product: 'Cell Phones',
+      carriers: [
+        'Verizon',
+        'Sprint',
+        'At&T'
+      ],
+      phone_models: {
+      apple: 'iPhone_6',
+      samsung: 'Galaxy_s6',
+      google: 'Nexus_6p'
+      }
+  },
+  aisle_two: {
+    product: 'Video Games',
+
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
