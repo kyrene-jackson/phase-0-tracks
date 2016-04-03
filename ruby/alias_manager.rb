8548cf59a1a4499283ebc31fe46c133ee688d2d4
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-03 16:32:52 -0500

    Create and test vowel and consonant methods

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,5 +7,12 @@ def next_vowel(word)
   word.tr!('aeiou', 'eioua')
 end
 
-p user_name
-p next_vowel(user_name)
+new_name = next_vowel(user_name)
+p new_name
+
+# Create a method that converts consonants to the next letter in the alphabet
+def next_consonant(word)
+  word.tr!('bcdfghjklmnpqrstvwxyz', 'cdfghjklmnpqrstvwxyz')
+end
+
+p next_consonant(new_name)
