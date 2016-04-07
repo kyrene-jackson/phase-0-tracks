479d308cceffa5466817fa6f8b35620bf17e7a2c
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-06 22:50:27 -0500

    Add outline in comments for puppy.rb

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+SPECIES---------------------
+Canis familiaris/lupus
+
+CHARACTERISTICS------------
+Breed: varies
+Friendly: yes
+Adorable: yes
+Legs: 4
+Eye count: 2
+Color: varies
+
+BEHAVIOR------------------
+Bark
+Lick
+Run 
