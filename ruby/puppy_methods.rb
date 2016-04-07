a74bacc4362926b7218830cac4ce65dac36e0750
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-07 14:16:51 -0500

    Create and test instance method 'speak'

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -2,8 +2,22 @@ class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
-    toy
+  end
+  def speak(number)
+    number.times { |number| puts "Woof!" }
   end
 
 end
 
+
+
+
+
+
+
+
+
+# DRIVER CODE
+practice = Puppy.new
+practice.fetch("ball")
+practice.speak(5)
