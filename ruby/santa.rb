35b67cab1cea9f9accd02f35e9387a50cd8fe717
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-07 21:58:29 -0500

    Add gender, and ethnic variations to add to Santa array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,12 @@
 #create a santaclass with 3 instance methods
 #methods: speak, eat_milk_and_cookies, and initialize
-
+#update santa with new attributes: gender, ethnicity, reindeer_ranking, and age
 class Santa
-  def initialize
-    puts "Initializing Santa instance..."
+  def initialize(gender, ethnicity)
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
   end
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
@@ -13,6 +16,15 @@ class Santa
   end
 end
 #Driver code
-practice = Santa.new
-practice.speak 
-practice.eat_milk_and_cookies("snickerdoodle")
+# practice = Santa.new
+# practice.speak
+# practice.eat_milk_and_cookies("snickerdoodle")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "gender fluid", "N/A"]
+example_ethnicities = ["Egyptian", "Scottish", "Indonesian", "Brazilian", "Multi-Ethnic", "Mystical Dragon", "Prefer not to say"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+p santas
