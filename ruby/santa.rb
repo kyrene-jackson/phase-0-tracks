2003ef50777bc9600118cf1b801b02c49adddd75
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-07 23:14:24 -0500

    Add pseudocode to driver code section, add two getter methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,4 @@
-#create a santaclass with 3 instance methods
+#create a santa class with 3 instance methods
 #methods: speak, eat_milk_and_cookies, and initialize
 #update santa with new attributes: gender, ethnicity, reindeer_ranking, and age
 class Santa
@@ -6,7 +6,7 @@ class Santa
     @gender = gender
     @ethnicity = ethnicity
     @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-    @age = 0
+    @age = 20
   end
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
@@ -14,17 +14,33 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
   end
+  #add two attribute changing methods to santa class
+  def celebrate_birthday
+
+  end
+  #add two getter methods: gender should return @gender, and ethnicity should return @ethnicity
+  def gender
+    @gender
+  end
+  def ethnicity
+    @ethnicity
+  end
 end
-#Driver code
+
+#=========DRIVER CODE=============================
+# TEST: initialize, speak, and eat_milk_and_cookies methods
 # practice = Santa.new
 # practice.speak
 # practice.eat_milk_and_cookies("snickerdoodle")
-
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "gender fluid", "N/A"]
-example_ethnicities = ["Egyptian", "Scottish", "Indonesian", "Brazilian", "Multi-Ethnic", "Mystical Dragon", "Prefer not to say"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
-
-p santas
+# TEST: gender and ethnicity array iteration
+# santas = []
+# example_genders = ["Agender", "Female", "Bigender", "Male", "Gender Fluid", "Prefer not to say"]
+# example_ethnicities = ["Egyptian", "Scottish", "Indonesian", "Brazilian", "Multi-Ethnic", "Mystical Dragon", "Prefer not to say"]
+# example_ethnicities.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
+# p santas
+# TEST: getter methods
+# practice = Santa.new("Female", "Brazilian")
+# p practice.gender
+# p practice.ethnicity
