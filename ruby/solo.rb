5aa4d56e0f3e0784a8609063e7fa832f536a3946
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-09 13:22:42 -0500

    Complete pesudocode for 6.4 solo challenge

---
+++ ruby/solo.rb
@@ -0,0 +1,22 @@
+#release 0
+#design a class
+#requirements ->
+  #at least three attributes (using at least two data types) whose values will vary from instance to instance (example: age)
+  #at least three methods, at least one of which takes an argument
+===================================================================
+#create wizard class
+#method 1: initialize(name)
+#method 2: cast spell(number of casts, spell type)
+#method 3: summon dragon(dragon color, dragon type)
+
+#attribute 1: name
+#attribute 2: spell type
+#attribute 3: number of casts
+#attribute 4: dragon color
+#attribute 5: dragon type
+
+#example output
+#initialize output: "(Kael'thas)"
+#cast spell output: "Kael'thas casted (3) (fire) spells!"
+#summon dragon output: "(Kael'thas) summoned a (crimson) (hydra)!"
+===================================================================
