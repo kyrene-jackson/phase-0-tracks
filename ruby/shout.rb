d358f821cf42bdf3a4472993dfc9c7b8ff4ea0fc
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-10 14:40:30 -0500

    Create shout module with yell_angrily and yelling_happily methods

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,2 +1,14 @@
 module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  def self.yelling_happily(words)
+    words + "!!!!!!!!" + " :D"
+  end
 end
+
+
+#Driver code
+
+puts Shout.yell_angrily("im mad")
+puts Shout.yelling_happily("yay")
