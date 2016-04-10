701f157a38c70c4753f0acb92147b03bc9cd44b3
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-09 19:20:02 -0500

    Reorganize pseudocode for each release, complete 6.4

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -1,11 +1,8 @@
 #=======================RELEASE 0===============================
 #create wizard class
 #methods: initialize, cast spell, summon dragon, and teleport
-#attribute 1: name
-#attribute 2: spell type
-#attribute 3: number of casts
-#attribute 4: dragon color
-#example output
+#attributes: name, spell type, number of casts, dragon color
+#example output:
 #cast spell output: "(Kael) casted (3) (fire) spells!"
 #summon dragon output: "(Kael) summoned a (crimson) (wyvern)!"
 #teleport: "(Kael) vanished..."
@@ -32,28 +29,20 @@ class Wizard
   end
 end
 #=======================RELEASE 2===============================
-#release 2: use class in a program
-#requirements ->
-  #Your user should be allowed to create as many instances of your class as they like.
-  #Prompt the user for each attribute, and don't forget to convert their input to the appropriate data type.
-  #Store these class instances in an array.
-  #When the user indicates that they are finished creating instances, loop through the array and print out the attributes of each instance as a confirmation message of what was created.
-
 #create empty array for storing instances
 #prompt user for each attribute
-#convert user input to proper data type. for example:
-#number_of_casts.to_i
+#note: dragon_type set as an attr_reader for rubric requirements
+#convert user input to proper data type
 #assign values to instance variables/attributes.
 #call class methods on new data, then print them in logical sentences
 #add values to result array
 #then ask if the user wants to continue or stop
-  #if stop -> iterate through array and list of attributes for each instance
-  #if continue -> restart loop
-
-
+#if no -> iterate through array and list of attributes for each instance
+#if yes -> restart loop
 result_array = []
 wants_to_continue = "yes"
-puts "Welcome to Create-a-Wiz where you will be making your own wizard complete with spells and its own pet dragon!"
+puts "Welcome to Create-a-Wiz!"
+puts "Make your own wizard complete with spells and its own pet dragon!"
 until wants_to_continue == "no"
 puts "Give your wizard a name"
 name = gets.chomp.capitalize
@@ -77,5 +66,5 @@ elsif status == "n"
   wants_to_continue = "no"
 end
 end
-
+puts "A history of instances created:"
 result_array.each { |result| p result }
