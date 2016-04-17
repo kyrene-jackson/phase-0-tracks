fabd6063e48f64ff0f6a52a80bfd3ae44398a056
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-17 18:34:22 -0500

    Create function that prints the longest string in an input array, test function in driver code

--- js/algos.js
+++ js/algos.js
@@ -1,17 +1,33 @@
-// write a function that takes an array of words or phrases,
-// and returns the longest word in the array.
-// example input: ['mint', 'gum', 'lollipop']
-// example output: "lollipop"
+/*
+write a function that takes an array of words or phrases,
+and returns the longest word in the array.
+example input: ['mint', 'gum', 'lollipop']
+example output: 'lollipop'
+step 1: create a new function that takes a parameter
+step 2: create a 'longestWord' variable that holds the longest string
+step 3: create a variable 'currentWord' that will hold the current string in the array
+step 4: FOR the length of the input array TIMES:
+     step 5: access first word by index and assign to 'currentWord'
+     step 6: compare the length of 'currentWord' against the length of 'longestWord'
+     step 7: IF 'currentWord' length is greater than 'longestWord' length
+     step 8: assign word to 'longestWord'
+step 9: Once last string is evaluated, print 'longestWord'
+*/
 
-// step 1: create a new function that takes a parameter
-// step 2: create a 'longestWord' variable that holds the value of the longest string
-// step 3: set 'longestWord' default value to " "
-// step 4: assign the length of the input array to variable 'arrLength'
-// step 5: create a variable 'currentWord' that will hold the current string in the array
-// step 6: FOR the length of the input array TIMES:
-//      step 7: access first word by index and assign to 'currentWord'
-//      step 8: compare the length of 'currentWord' against the length of 'longestWord'
-//      step 9: IF 'currentWord' length is greater than 'longestWord' length
-//      step 10: assign word to 'longestWord'
-//      step 11: ELSE move on to next string
-// step 12: Once last string is evaluated, print 'longestWord'
+// DRIVER CODE
+var longestWord = null;
+var currentWord = null;
+
+
+function findWord(array) {
+  for (var i = 0; i < array.length; i++) {
+  longestWord = ""
+  currentWord = array[i]
+    if (currentWord.length > longestWord.length) {
+      longestWord = currentWord
+    }
+  }
+  console.log("The longest word is: " + longestWord + ".");
+}
+
+findWord(['mint', 'gum', 'lollipop']);
