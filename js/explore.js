29dfbb575c0cd708310979a2f404ef8a3c7fdda8
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-16 22:34:37 -0500

    Pseudocode 7.1 release 4

---
+++ js/explore.js
@@ -0,0 +1,13 @@
+// In comments, pseudocode a function that takes a string as a parameter and reverses the string. reverse("hello"), for
+// example, should return "olleh". This isn't a task specific to JavaScript -- your pseudocode should be in plain English // and use phrases like "for each" instead of JavaScript-specific terms. Remember that you can add strings in JavaScript, // so '' + 'a' would result in the string 'a'. Take your time. This is the most valuable skill in programming: being able // to think through a problem logically.
+
+// Create a reverse method that takes a string as a perameter.
+// User inputs a string. Example: "hello".
+// Assign user input to variable.
+// Assign empty string to a variable. Example: a = " ";
+// Determine the number of letters in input string.
+// For each number in the count greater than 0,
+// print a single letter from the string begining from the end.
+// Add variable set to empty string to each letter.
+// Once counter is greater than or equal to 0,
+// Print the complete string.
