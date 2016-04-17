dc7eb9ee0a5a595d4494ac96090dad50f172ba9b
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-04-17 17:36:37 -0500

    Create algos.js, pseudocode challenge

---
+++ js/algos.js
@@ -0,0 +1,17 @@
+// write a function that takes an array of words or phrases,
+// and returns the longest word in the array.
+// example input: ['mint', 'gum', 'lollipop']
+// example output: "lollipop"
+
+// step 1: create a new function that takes a parameter
+// step 2: create a 'longestWord' variable that holds the value of the longest string
+// step 3: set 'longestWord' default value to " "
+// step 4: assign the length of the input array to variable 'arrLength'
+// step 5: create a variable 'currentWord' that will hold the current string in the array
+// step 6: FOR the length of the input array TIMES:
+//      step 7: access first word by index and assign to 'currentWord'
+//      step 8: compare the length of 'currentWord' against the length of 'longestWord'
+//      step 9: IF 'currentWord' length is greater than 'longestWord' length
+//      step 10: assign word to 'longestWord'
+//      step 11: ELSE move on to next string
+// step 12: Once last string is evaluated, print 'longestWord'
