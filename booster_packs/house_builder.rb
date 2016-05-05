70933c0df1b62ee5ed45c0216cc94e8eafae4054
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-05 14:04:51 -0500

    Complete main loop for adding rooms

--- booster_packs/house_builder.rb
+++ booster_packs/house_builder.rb
@@ -1,11 +1,11 @@
+# Week 5 Booster: House Builder!
 # Allow the user to create a house,
 # then add rooms,
 # then add items
-
 # House can have up to 5 rooms
 # Room can have an unlimited number of items
 
-# BUSINESS LOGIC
+#============= BUSINESS LOGIC ================
 
 def add_room_to_house!(house, room_name)
   if house.keys.length == 5
@@ -16,24 +16,47 @@ def add_room_to_house!(house, room_name)
   end
 end
 
+def add_item_to_room!(house, room_name, item_name)
+  house[room_name] << item_name
+end
+
 house = {}
 
-# USER INTERFACE
+#============= USER INTERFACE ================
 
 def print_house(house)
-  p house
+  puts "--------------"
+  puts "Current house configuration"
+  house.each do |room_name, items|
+    puts "#{room_name}: #{items}"
+  end
+  puts "-------------- "
 end
 
 
+# Let the user add rooms
+can_add_rooms = true
 
-
-
-# TEST CODE
-
-rooms = ["Living room", "bedroom", "bathroom", "kitchen", "bedroom 2", "bedroom 3"]
-
-rooms.each do |room|
-  add_room_to_house!(house, room)
+# Stop loop when rooms cannot be added
+while can_add_rooms
+    # Get a room name from the user
+    puts "Type the name of a room to add (or type 'done'):"
+    room_name = gets.chomp
+    # If the user is done, stop loop
+    break if room_name == 'done'
+    # Otherwise, add the room to the house
+    can_add_rooms = add_room_to_house!(house, room_name)
+    print_house(house)
 end
 
-print_house(house)
+
+#============= TEST CODE ===================
+
+# rooms = ["Living room", "bedroom", "bathroom", "kitchen", "bedroom 2"]
+#
+# rooms.each do |room|
+#   add_room_to_house!(house, room)
+#   add_item_to_room!(house, room, "cat")
+# end
+#
+# print_house(house)
