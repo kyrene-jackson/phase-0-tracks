8996921a0c3fa1f264ce22ccf7b7293ca403b8f0
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-05 19:06:28 -0500

    Create room class, test room and item classes

--- booster_packs/classy/room.rb
+++ booster_packs/classy/room.rb
@@ -9,3 +9,35 @@
 # setter for items, name
 # total_value (adds up prices of items)
 # - to_s override
+
+require_relative "item"
+
+
+class Room
+  attr_reader :length, :width
+  attr_accessor :items, :name
+  def initialize(name, width, length)
+    @name = name
+    @width = width
+    @length = length
+    @items = []
+  end
+  def total_value
+    total = 0
+    @items.each do |item|
+      total += item.price
+    end
+    total
+  end
+  def to_s
+    "#{@name} (#{@width} x #{@length})"
+  end
+end
+
+living_room = Room.new("Living room", 20, 35)
+puts living_room
+piano = Item.new("Piano", "black", 10000)
+box = Item.new("cardboard box", "brown", 0)
+living_room.items << piano
+living_room.items << box
+puts living_room.total_value
