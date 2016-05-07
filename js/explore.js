7516e38f09e9b0f9bdbdb3aaf046002e0cb7b646
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-07 00:11:44 -0500

    Pseudocode challenge

---
+++ js/explore.js
@@ -0,0 +1,13 @@
+// 7.1 Release 4
+// Write a JavaScript function that
+// takes a string as a parameter,
+// and reverses the string.
+
+// Declare function: 'reverse'
+// that takes a word as a parameter.
+// Store word in a variable.
+// Starting from the last letter of the word:
+  // Take each letter and store it in a variable.
+// When finished, add each letter together,
+// to form a single word.
+// Print final word.
