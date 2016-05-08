2cf7c8edf52f10824086d10bb6f0c77e81224d71
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-08 18:59:11 -0500

    Restructure pseudocode, remove current word variable, refactor findword function

--- js/algos.js
+++ js/algos.js
@@ -5,26 +5,22 @@ and returns the longest word in the array.
 example input: ['mint', 'gum', 'lollipop']
 function should return: 'lollipop'
 step 1: create a new function that takes a parameter
-step 2: create a 'longestWord' variable that holds the longest string
-step 3: create a variable 'currentWord' that will hold the current string in the array
-step 4: FOR the length of the input array TIMES:
-     step 5: access first word by index and assign to 'currentWord'
-     step 6: compare the length of 'currentWord' against the length of 'longestWord'
-     step 7: IF 'currentWord' length is greater than 'longestWord' length
-     step 8: assign word to 'longestWord'
-step 9: Once last string is evaluated, print 'longestWord'
-*/
-
-
-
+step 2: create a 'longestWord' variable that holds
+the longest string
+step 3: FOR the length of the input array TIMES:
+           step 4: access first word by index
+           step 5: compare the length of index selected word
+           against the length of 'longestWord'
+           step 6: IF word is length is greater than 'longestWord' length
+           step 7: assign word to 'longestWord'
+step 8: Once last string is evaluated, print 'longestWord'
+=======================================================================*/
 function findWord(array) {
   var longestWord = null;
-  var currentWord = null;
   for (var i = 0; i < array.length; i++) {
   longestWord = ""
-  currentWord = array[i]
-    if (currentWord.length > longestWord.length) {
-      longestWord = currentWord
+    if (array[i].length > longestWord.length) {
+      longestWord = array[i]
     }
   }
   console.log("The longest word is: " + longestWord + ".");
