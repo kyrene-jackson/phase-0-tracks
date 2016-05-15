7dcc500c4434c041eeb185f36bec1254c99343ca
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-15 18:58:52 -0500

    Pegin pseudocoding dataclass, transfer code from main program file to data class

--- databases/videogame_manager/videogame_manager.rb
+++ databases/videogame_manager/videogame_manager.rb
@@ -11,25 +11,50 @@ require 'sqlite3'
 require_relative 'managerclass'
 
 #===========================================
+# Create new database
 $DATABASE = SQLite3::Database.new("videogames.db")
 
+# Create new tables
+create_console_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS console (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(255)
+  )
+SQL
 
+create_genre_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS genre (
+    id INTEGER PRIMARY KEY,
+    type VARCHAR(255)
+  )
+SQL
 
+create_game_table = <<-SQL
+  CREATE TABLE IF NOT EXISTS game (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(255),
+    console_id INT,
+    genre_id INT,
+    price DECIMAL(4, 2),
+    completed BOOLEAN,
+    FOREIGN KEY (console_id) REFERENCES console(id),
+    FOREIGN KEY (genre_id) REFERENCES genre(id)
+  )
+SQL
 
-
-
+# Execute tables
+$DATABASE.execute(create_console_table)
+$DATABASE.execute(create_genre_table)
+$DATABASE.execute(create_game_table)
 
 #============DRIVER CODE==============
-# initialize new instance
+# initialize new instance of both classes
 new_library = GameManager.new("Kyrene")
 
 
 
 
-# Execute tables
-# $DATABASE.execute(create_console_table)
-# $DATABASE.execute(create_genre_table)
-# $DATABASE.execute(create_game_table)
+
 
 
 
