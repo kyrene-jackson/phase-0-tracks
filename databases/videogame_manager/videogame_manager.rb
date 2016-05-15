9744d634980f889607a5fd9ef39db856485c29cc
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-14 20:10:59 -0500

    Begin pseudocoding program

---
+++ databases/videogame_manager/videogame_manager.rb
@@ -0,0 +1,15 @@
+# Video Game Manager
+# A program that allows you
+# to organize your video game library
+# from the command line!
+# Programmed By: Kyrene Jackson
+#
+#=====================================
+# PSEUDO CODE
+# Step: create SQLite3 database
+
+
+
+
+
+#=====================================
