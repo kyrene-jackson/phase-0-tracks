62baef05a5a290204f8a25d8f9592af7e9d91d98
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-15 19:00:51 -0500

    initialize new instances of both data and game manager classes, test for errors

--- databases/videogame_manager/videogame_manager.rb
+++ databases/videogame_manager/videogame_manager.rb
@@ -9,47 +9,12 @@
 #=============REQUIRES====================
 require 'sqlite3'
 require_relative 'managerclass'
-
-#===========================================
-# Create new database
-$DATABASE = SQLite3::Database.new("videogames.db")
-
-# Create new tables
-create_console_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS console (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(255)
-  )
-SQL
-
-create_genre_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS genre (
-    id INTEGER PRIMARY KEY,
-    type VARCHAR(255)
-  )
-SQL
-
-create_game_table = <<-SQL
-  CREATE TABLE IF NOT EXISTS game (
-    id INTEGER PRIMARY KEY,
-    title VARCHAR(255),
-    console_id INT,
-    genre_id INT,
-    price DECIMAL(4, 2),
-    completed BOOLEAN,
-    FOREIGN KEY (console_id) REFERENCES console(id),
-    FOREIGN KEY (genre_id) REFERENCES genre(id)
-  )
-SQL
-
-# Execute tables
-$DATABASE.execute(create_console_table)
-$DATABASE.execute(create_genre_table)
-$DATABASE.execute(create_game_table)
+require_relative 'dataclass'
 
 #============DRIVER CODE==============
 # initialize new instance of both classes
-new_library = GameManager.new("Kyrene")
+test_user = GameManager.new("Kyrene")
+test_database = GameDatabase.new
 
 
 
@@ -57,18 +22,6 @@ new_library = GameManager.new("Kyrene")
 
 
 
-
-
-#============USER INTERFACE===========
-
-
-
-#
-# # MENU
-# puts "Select a command: "
-
-# menu_selection = gets.chomp
-# begin case statement
 
 
 #============TEST CODE=================
