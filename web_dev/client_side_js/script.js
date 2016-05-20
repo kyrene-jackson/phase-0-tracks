284b312a39af6d58eef277b49d5ad81ff4feb3c8
kyrene-jackson <kyrene_jackson@hotmail.com>
2016-05-20 16:36:01 -0500

    Add basic dom manipulation

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,7 +1,16 @@
+// Change background color of main title to red
+
 function changeBackground(event) {
   console.log(event)
   event.target.style.background = "red";
 }
 
+// Loop through abilities list and give each list item a class name
+
 var title = document.getElementById('heading')
 title.addEventListener("click", changeBackground);
+
+var listItems = document.querySelectorAll('li');
+for (var i = 0; i < listItems.length; i++) {
+  listItems[i].className = 'abilities';
+}
